c06a1992ffc8188e30ac66e993d4afb031ea46c0
nndegwa <nelson.ndegwa@gmail.com>
2024-02-15 10:16:26 +0100

    init commit

---
+++ setup.R
@@ -0,0 +1,18 @@
+# Project setup
+
+# One off installation
+# pak::pkg_install(
+#   c("arrow", "babynames", "curl", "duckdb", "gapminder",
+#     "ggrepel", "ggridges", "ggthemes", "hexbin", "janitor", "Lahman",
+#     "leaflet", "maps", "nycflights13", "openxlsx", "palmerpenguins",
+#     "repurrrsive", "tidymodels", "writexl", "here", "survival",
+#     "styler", "lintr", "pacman")
+# )
+
+library(tidyverse)
+library(tidyverse)
+library(ggthemes)
+library(here)
+library(styler)
+library(nycflights13)
+
